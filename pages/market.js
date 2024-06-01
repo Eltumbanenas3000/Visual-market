b0290905daaed93542f0ddddcda16b912e96cd50
carlosalexisart@gmai.com <carlosalexisart@gmail.com>
2024-06-01 21:32:34 +0200

    sexto commit

--- pages/market.js
+++ pages/market.js
@@ -39,7 +39,7 @@ export default function Home() {
                     </div>
                     <h3 className="text-2xl font-bold mb-2">Hey, welcome to my site!</h3>
                     <p>Im a digital designer, developer, and surfer from Malibu. In the digital world, I craft interfaces and dive into coding intricacies.</p>
-                    <p className="mt-2">Beyond screens, I find solace and joy riding waves along Malibu's shores. My life blends tech and nature, coding sessions, and catching waves.</p>
+                    <p className="mt-2">Beyond screens, I find solace and joy riding waves along Malibu&apos;s shores. My life blends tech and nature, coding sessions, and catching waves.</p>
                 </div>
               
                 <div className="bg-white dark:bg-zinc-800 p-6 rounded-lg shadow-lg text-center">
@@ -92,11 +92,11 @@ export default function Home() {
                 <div className="bg-white dark:bg-zinc-800 p-6 rounded-lg shadow-lg">
                     <h3 className="text-2xl font-bold mb-4">Testimonials</h3>
                     <blockquote className="mb-4">
-                        <p className="italic">"Big fan of your themes. They're well-organized, look clean, and are fast."</p>
+                        <p className="italic">Big fan of your themes. They&apos;re well-organized, look clean, and are fast.</p>
                         <footer className="text-sm text-right">- Kevin Fossil, Developer</footer>
                     </blockquote>
                     <blockquote>
-                        <p className="italic">"Amazing resource using @astrotobuild and @tailwindcss! I got it a week ago and have found the contents really useful. 5/5"</p>
+                        <p className="italic">Amazing resource using @astrotobuild and @tailwindcss! I got it a week ago and have found the contents really useful. 5/5</p>
                         <footer className="text-sm text-right">- James, Entrepreneur</footer>
                     </blockquote>
                 </div>
