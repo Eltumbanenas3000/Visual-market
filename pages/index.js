1a268f17eabfe99adc1d7cb8c1be817834e2de3f
Eltumbanenas3000 <carlosalexisart@gmail.com>
2024-06-19 06:37:38 +0200

    De css puro a tailwind y responsive

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 import React from 'react';
 
-
 export default function Home() {
-    return (
-      <>
-        <nav className="navbar">
+  return (
+    <>
+      <nav className="navbar bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
           <div className="logo">
             <Image
               src="/visual crafts banco.png"
@@ -16,189 +16,120 @@ export default function Home() {
             />
           </div>
           <div>
-            <ul>
-              <li><a href="#">Inicio</a></li>
-              <li><a href="#nosotros">Nosotros</a></li>
-              <li><a href="#contacto">Contacto</a></li>
-              <li><a href="#productos">Productos</a></li>
-              <li><a href="#precio">Precio</a></li>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="#">
+                  Inicio
+                </Link>
+              </li>
+              <li>
+                <Link href="#nosotros">
+                  Nosotros
+                </Link>
+              </li>
+              <li>
+                <Link href="#contacto">
+                  Contacto
+                </Link>
+              </li>
+              <li>
+                <Link href="#productos">
+                  Productos
+                </Link>
+              </li>
+              <li>
+                <Link href="#precio">
+                  Precio
+                </Link>
+              </li>
             </ul>
           </div>
-        </nav>
-        <header className="Nosotros"></header>
-        <section className="info" id="productos">
-          <div className="firstbox">
-            <h2 className="title">Productos</h2>
-            <p className="info">
-              Descubre la magia de la personalización con Visual Crafts.
-              Desde playeras y sudaderas hasta termos grabados a láser,
-              letreros LED y mucho más, cada producto es una obra maestra
-              única. Convierte tus ideas en realidad y deja una impresión
-              duradera con nuestros volantes, tarjetas de presentación y más.
-              ¡Explora nuestra colección de productos personalizados y haz
-              que tu marca destaque con estilo y originalidad!
-            </p>
-  
-            <div className="box-container">
-              <div className="box">
-                <h3>Sellos</h3>
-                <Image
-                  src="/sellos.jpg"
-                  width={300}
-                  height={100}
-                  alt="sellos"
-                />
-                <p>Agiliza tus tiempos</p>
-              </div>
-              <div className="box">
-                <h3>Letreros </h3>
-                <Image
-                  src="/lamparas.jpg"
-                  width={300}
-                  height={100}
-                  alt="lamparas"
-                />
-                <p>Decora tu espacio</p>
-              </div>
-              <div className="box">
-                <h3>Sudaderas</h3>
-                <Image
-                  src="/sudaderas.jpg"
-                  width={300}
-                  height={100}
-                  alt="logo"
-                />
-                <p>Abrigate con estilo</p>
-              </div>
-              <div className="box">
-                <h3>Playeras</h3>
-                <Image
-                  src="/playeras.jpg"
-                  width={300}
-                  height={100}
-                  alt="playeras"
-                />
-                <p>Crea tu propia moda</p>
-              </div>
-              <div className="box">
-                <h3>Termos</h3>
-                <Image
-                  src="/termos.jpg"
-                  width={300}
-                  height={100}
-                  alt="termos"
-                />
-                <p>Hidratate</p>
-              </div>
-              <div className="box">
-                <h3>Volantes</h3>
-                <Image
-                  src="/volantes.jpeg"
-                  width={300}
-                  height={100}
-                  alt="volantes"
-                />
-                <p>Crea un identidad visual</p>
-              </div>
-            </div>
+        </div>
+      </nav>
+      <header className="Nosotros"></header>
+      <section className="info bg-zinc-900 text-white py-8" id="productos">
+        <div className="container mx-auto">
+          <div className="text-center py-4">
+            <h1 className="text-3xl font-bold">PRODUCTOS</h1>
+            <p className="mt-4 px-4">Descubre la magia de la personalización con Visual Craft. Desde playeras y sudaderas hasta termos grabados a láser, letreros LED y mucho más, cada producto es una creación única. Convierte las ideas en realidad y deja que impriman duradera en nuestra vida diaria, trabajo de personalización y más. Explora nuestro catálogo de productos personalizados y haz que tu marca destaque con estilo y originalidad.</p>
           </div>
-        </section>
-  
-        <section className="about" id="nosotros">
-          <h2 className="titulos">Nosotros</h2>
-          <p className="info">
-            Misión:
-            Crear productos personalizados de alta calidad que reflejen
-            la singularidad de cada cliente, inspirando a través de la
-            creatividad y la originalidad.</p>
-          <p>
-            Visión:{}
-            Ser líderes en la industria de productos personalizados,
-            destacando por nuestra calidad, innovación y crecimiento
-            continuo tanto a nivel nacional como internacional.
-          </p>
-  
-  
-        </section>
-  
-        <section className="precio" id="precio">
-          <article className="contain">
-            <h2 className="titulos">precio</h2>
-            <p className="info">
-              En Visual Crafts, nos enorgullece ofrecer productos personalizados
-              de alta calidad a precios moderados y asequibles. Creemos que todos
-              deberían tener acceso a artículos únicos y duraderos que reflejen
-              su estilo personal, sin comprometer la calidad. Por ello, nos esforzamos
-              por mantener nuestros precios accesibles para satisfacer las necesidades
-              y expectativas de nuestros clientes.
-            </p>
-            <div className="text-center py-8">
-                <Link href="/market" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 transform hover:scale-105 shadow-lg">
-                    Tienda
-                </Link>
-            </div>
-          </article>
-  
-        </section>
-        <section className="contacto" id="contacto">
-          <h2 className="title">contacto</h2>
-  
-          <div className="information">
-            <div className="mapita">
-  
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1273.2548150856276!2d-106.10661545388355!3d28.725934243849153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea41793a4dbb0b%3A0xd25631ac612aff8b!2sRafael%20Bernal%2015117%2C%20Jos%C3%A9%20Mar%C3%ADa%20Ponce%20de%20Le%C3%B3n%2C%20Quintas%20Carolinas%2C%2031136%20Chihuahua%2C%20Chih.!5e0!3m2!1ses-419!2smx!4v1713334724738!5m2!1ses-419!2smx"
-                width="500"
-                height="430"
-                style={{ border: "0" }}
-                allowFullScreen=""
+          <div className="flex flex-wrap justify-center py-4">
+            {[
+              { src: "/sellos.jpg", title: "Sellos", description: "Agiliza tus tiempos" },
+              { src: "/neon.jpg", title: "Letreros", description: "Decora tu espacio" },
+              { src: "/sudaderas.jpg", title: "Sudaderas", description: "Abrígate con estilo" },
+              { src: "/playeras.jpg", title: "Playeras", description: "Crea tu propia moda" },
+              { src: "/termos.jpg", title: "Termos", description: "Hidrátate" },
+              { src: "/volantes.jpeg", title: "Volantes", description: "Crea una identidad visual" }
+            ].map((product, index) => (
+              <div key={index} className="m-4">
+                <Image src={product.src} alt={product.title} width={150} height={150} className="rounded-lg" />
+                <h2 className="text-xl font-bold mt-2">{product.title}</h2>
+                <p>{product.description}</p>
+              </div>
+            ))}
+          </div>
+          <div className="text-center py-4">
+            <h1 className="text-3xl font-bold">NOSOTROS</h1>
+            <p className="mt-4 px-4">Misión: Crear productos personalizados de alta calidad que reflejen la originalidad de cada cliente, respetando a través de la creatividad y la originalidad.</p>
+            <p className="mt-4 px-4">Visión: Ser líderes en la industria de productos personalizados, destacando por nuestra calidad, innovación y crecimiento tanto a nivel nacional como internacional.</p>
+          </div>
+          <div className="precio">
+          <div className="text-center py-4" id="precio">
+            <h1 className="text-3xl font-bold">PRECIO</h1>
+            <p className="mt-4 px-4">En Visual Craft, nos enorgullece ofrecer productos personalizados de alta calidad a precios accesibles y asequibles. Creemos que todos deberían tener acceso a artículos únicos y exclusivos que reflejen su estilo, sin comprometer el bolsillo. Además, nos esforzamos por mantener nuestros precios justos y transparentes, para que siempre sepas exactamente lo que estás pagando.</p>
+            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300">Tienda</button>
+          </div>
+          </div>
+          <div className="text-center py-4" id="contacto">
+            <h1 className="text-3xl font-bold">CONTACTO</h1>
+            <div className="flex flex-col md:flex-row justify-center items-center mt-4 space-x-0 md:space-x-8 space-y-4 md:space-y-0">
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509374!2d144.95373531531585!3d-37.81627917975195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d6b1e4e4e0e1!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1633076374008!5m2!1sen!2sau" 
+                width="400" 
+                height="300" 
+                style={{ border: 0 }} 
+                allowFullScreen="" 
                 loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
-  
-            </div>
-            <div className="form-content">
-  
-              <form method="" className="form-contacto">
-                <label for="name">Nombre</label>
-                <input style={{ width: "100%" }} type="text" name="name" id="name" />
-  
-                <label for="email">Correo electronico</label>
-                <input type="email" name="subjet" id="subjet" />
-  
-                <label for="subjet">Asunto</label>
-                <input type="text" name="subjet" id="subjet" />
-  
-                <label for="coments">Comentarios</label>
-                <textarea name="coments" id="coments" cols="30" rows="5"></textarea>
-  
-                <button className="btn-enviar">Enviar</button>
+              <form className="mt-2 max-w-md w-full">
+                <div className="mb-4">
+                  <label className="block text-left" htmlFor="name">Nombre</label>
+                  <input className="w-full p-2 border border-zinc-300 rounded-lg" type="text" id="name" name="name" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-left" htmlFor="email">Correo electrónico</label>
+                  <input className="w-full p-2 border border-zinc-300 rounded-lg" type="email" id="email" name="email" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-left" htmlFor="subject">Asunto</label>
+                  <input className="w-full p-2 border border-zinc-300 rounded-lg" type="text" id="subject" name="subject" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-left" htmlFor="comments">Comentarios</label>
+                  <input className="w-full p-2 border border-zinc-300 rounded-lg" type="text" id="comments" name="comments" />
+                </div>
+                <button className="bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-black transition-colors duration-300">Enviar</button>
               </form>
             </div>
           </div>
-        </section>
-
-        <footer className="py-10 bg-zinc-800 text-center">
-                <div className="border-t border-zinc-700 mt-4 mb-6 pt-4 flex justify-center">
-                    <div className="flex space-x-4">
-                        <Link href="https://www.facebook.com/?locale=es_LA" passHref>
-                            
-                                <Image alt="Facebook" src="/facebook.png" width={40} height={40} />
-                            
-                        </Link>
-                        <Link href="https://github.com/Eltumbanenas3000" passHref>
-                            
-                                <Image alt="Github" src="/github.png" width={40} height={40} />
-                            
-                        </Link>
-                        <Link href="https://www.instagram.com/visual._.crafts_?igsh=cmtubDdlNnV0MDk%3D&utm_source=qr" passHref>
-                            
-                                <Image alt="instagram" src="/instagram.png" width={40} height={40} />
-                            
-                        </Link>
-                    </div>
-                </div>
-            </footer>
-      </>
-    );
-  }
+        </div>
+      </section>
+      <footer className="py-10 bg-zinc-800 text-center">
+        <div className="container mx-auto border-t border-zinc-700 mt-4 mb-6 pt-4 flex justify-center">
+          <div className="flex space-x-4">
+            <Link href="https://www.facebook.com/?locale=es_LA" passHref>
+              <Image alt="Facebook" src="/facebook.png" width={40} height={40} />
+            </Link>
+            <Link href="https://github.com/Eltumbanenas3000" passHref>
+              <Image alt="Github" src="/github.png" width={40} height={40} />
+            </Link>
+            <Link href="https://www.instagram.com/visual._.crafts_?igsh=cmtubDdlNnV0MDk%3D&utm_source=qr" passHref>
+              <Image alt="instagram" src="/instagram.png" width={40} height={40} />
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
