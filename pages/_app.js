be39ac41196512937c18734c16575b7309626530
Eltumbanenas3000 <carlosalexisart@gmail.com>
2024-06-21 23:33:20 +0200

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+// pages/_app.js o pages/_app.tsx (dependiendo de si usas JavaScript o TypeScript)
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,35 +1,26 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "../../../libs/mongodb";
+import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async session({ session, token, user }) {
-      console.log("Session callback:", session);
-      return session;
-    },
-  },
-  debug: true,
-};
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET
+          }),
+        // ...add more providers here
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
