93c97765b57f9268febbfa1de9cc9a7e85f6727c
Eltumbanenas3000 <carlosalexisart@gmail.com>
2024-06-27 05:05:46 +0200

    sigo optimizando la pagina

--- pages/market.js
+++ pages/market.js
@@ -32,7 +32,7 @@ export default function Home() {
             <section>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 bg-zinc-950 dark:bg-zinc-900 text-zinc-200 shadow">
             <Link href="/bottles">
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-20 duration-300 text-center cursor-pointer">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300 text-center cursor-pointer">
                     <div className="flex flex-col items-center mb-4">
                         <h2 className="text-3xl font-bold mb-4">Termos</h2>
                         <Image
@@ -46,13 +46,13 @@ export default function Home() {
                 </div>
             </Link>
             <Link href="/bottles">
-            <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-20 duration-300 text-center">
+            <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300 text-center">
            
                     <h3 className="text-3xl font-bold mb-4">Playeras</h3>
-                    <div className="flex justify-center space-x-2">
+                    <div className="flex justify-center space-x-3 mt-7 mb-7">
                     <Image
                             src="/playerasmarket.jpg"
-                            width={400}
+                            width={700}
                             height={300}
                             alt="termos"
                             className="rounded-lg"
@@ -61,12 +61,12 @@ export default function Home() {
                 </div>
             </Link>
             <Link href="/bottles">
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-20 duration-300 text-center">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300 text-center">
                     <h3 className="text-3xl font-bold mb-4">Sellos</h3>
                     <div className="flex justify-center">
                     <Image
                             src="/sellosmarket.jpg"
-                            width={355}
+                            width={340}
                             height={50}
                             alt="termos"
                             className="rounded-lg"
@@ -75,7 +75,7 @@ export default function Home() {
                 </div>
             </Link>
             <Link href="/bottles">
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-20 duration-300">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300">
                 <h3 className="text-3xl font-bold ">Letreros neon</h3> 
                         <div className="flex justify-center">
                     <Image
@@ -89,7 +89,7 @@ export default function Home() {
                 </div>
             </Link>
                 <Link href="/bottles">
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-20 duration-300">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300">
                     <h3 className="text-2xl font-bold mb-2">Imprimibles</h3>
                     <div className="flex justify-center">
                     <Image
@@ -103,7 +103,7 @@ export default function Home() {
                 </div>
                 </Link>
                 <Link href="/bottles">
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-20 duration-300 text-center">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300 text-center">
                 <h3 className="text-2xl font-bold mb-2">Letreros de madera</h3>
                     <div className="flex justify-center">
                 <Image
@@ -116,7 +116,7 @@ export default function Home() {
                     </div>
                 </div>
                 </Link>
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-105 duration-300 text-center">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300 text-center">
                     <h3 className="text-2xl font-bold mb-4">Catalogo</h3>
                     <form className="flex flex-col space-y-2">
                         <input type="email" placeholder="Enter your email" className="p-2 rounded-lg border border-zinc-300 dark:border-zinc-700" />
@@ -124,7 +124,7 @@ export default function Home() {
                     </form>
                 </div>
 
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-105 duration-300">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300">
                     <h3 className="text-2xl font-bold mb-4">Testimonios</h3>
                     <blockquote className="mb-4">
                         <p className="italic">text.</p>
@@ -136,7 +136,7 @@ export default function Home() {
                     </blockquote>
                 </div>
 
-                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-105 duration-300">
+                <div className="bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-300">
                     <h3 className="text-2xl font-bold mb-4">Proveedores</h3>
                     <p>text</p>
                 </div>
@@ -150,11 +150,11 @@ export default function Home() {
 
             <section className="py-16 bg-zinc-900 text-zinc-200">
             
-                <h2 className="text-center text-3xl font-bold mb-5">Sericios</h2>
+                <h2 className="text-center text-3xl font-bold mb-5">Servicios</h2>
                 <h3 className="text-center font-bold mb-8">Los servicio pueden variar de precio
                 dependiendo de la fase en la que se encuentre tu marca nosotros nos adaptamos para desarrollarla desde donde esté</h3>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4">
-                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-105 duration-300">
+                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-600">
                     <div className="flex justify-center items-center"> 
                         <Image
                             src="/chante.jpg"
@@ -170,7 +170,7 @@ export default function Home() {
                     Destaca entre la multitud con un logo que realmente te represente.</p>
 
                     </div>
-                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-105 duration-300">
+                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-600">
                     <Image
                             src="/branding.jpg"
                             width={400}
@@ -180,9 +180,9 @@ export default function Home() {
                         />
                         <h3 className="text-3xl font-bold mb-4 mt-4 text-white-900">Branding</h3>
                         <p className="text-white-300">Desarrollamos el aspecto visual de la marca y hacemos un estudio mercadológico para que solo tu 
-                        descanses y disfrutes como los numeros y el renombre de tu marca sube como la espuma.</p>
+                        descanses y el renombre de tu marca suba como la espuma.</p>
                     </div>
-                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-105 duration-300">
+                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-1 transition-transform transform hover:scale-20 duration-600">
                         <Image
                             src="/identidadvisual.jpg"
                             width={400}
@@ -192,9 +192,9 @@ export default function Home() {
                         />
                         <h3 className="text-3xl font-bold mb-4 mt-4 text-white-900">Identidad visual</h3>
                         <p className="text-white-300">Desarrollamos el aspecto visual de tu marca para que enamores a tu mercado primero por los ojos, 
-                        creamos visuales de calidad para que tu marca no pierdad la esencia</p>
+                        creamos visuales de calidad sin perder la esencia.</p>
                     </div>
-                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-4 transition-transform transform hover:scale-105 duration-300">
+                    <div className="text-center bg-zinc-800 p-6 rounded-lg shadow-lg hover:shadow-blue-500/100 hover:m-2 transition-transform transform hover:scale-20 duration-600">
                         <Image
                             src="/brandbord.jpg"
                             width={400}
